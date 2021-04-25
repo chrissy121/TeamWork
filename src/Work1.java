7e9e06ccf9716928f7b9648aaad1e33ea107e27b
Chrissy121 <crys121@gmail.com>
2021-04-24 22:47:32 -0400

    2 team member came to in zoom

--- src/Work1.java
+++ src/Work1.java
@@ -1,5 +1,6 @@
 public class Work1 {
     public static void main(String[] args) {
         System.out.println("Gokay is very angry today and Oogii is very productive, Mindy is so lazy, I am very damn");
+        System.out.println("Samed is very fresh and Tatiana is in Nadir's house");
     }
 }
