5d88323119faab78afc3a7d212b1bee75031dfb7
Chrissy121 <crys121@gmail.com>
2021-04-24 22:43:43 -0400

    Finally we work together

---
+++ src/Work1.java
@@ -0,0 +1,5 @@
+public class Work1 {
+    public static void main(String[] args) {
+        System.out.println("Gokay is very angry today and Oogii is very productive, Mindy is so lazy, I am very damn");
+    }
+}
